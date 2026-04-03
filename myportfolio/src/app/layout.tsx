d6dd7ec81e606e735652bf286982bff3ed50c666
Baha Alimi <a.alimi@innopolis.university>
2026-04-03 14:17:28 +0300

    HERE U GO DOCTOR LIVSEY

--- myportfolio/src/app/layout.tsx
+++ myportfolio/src/app/layout.tsx
@@ -13,15 +13,21 @@ const geistMono = Geist_Mono({
 });
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
-const windowIconPath = `${basePath}/icons/DoctorLiveseyR.png`;
+const iconVersion = "20260403";
+const pngIconPath = `${basePath}/icons/doctor-livsey-favicon-64.png?v=${iconVersion}`;
+const icoIconPath = `${basePath}/favicon.ico?v=${iconVersion}`;
+const appleIconPath = `${basePath}/icons/doctor-livsey-apple-180.png?v=${iconVersion}`;
 
 export const metadata: Metadata = {
   title: "3llimi",
   description: "This is the Portfolio of Ahmed Baha Eddine Alimi",
   icons: {
-    icon: windowIconPath,
-    shortcut: windowIconPath,
-    apple: windowIconPath,
+    icon: [
+      { url: pngIconPath, sizes: "64x64", type: "image/png" },
+      { url: icoIconPath, type: "image/x-icon" },
+    ],
+    shortcut: icoIconPath,
+    apple: [{ url: appleIconPath, sizes: "180x180", type: "image/png" }],
   },
 };
 
